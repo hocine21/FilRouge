f9dd1365525d1b4ce412de3ca00f57e85d53403a
Nip1706 <payagalage.nipuna@gmail.com>
2024-06-27 14:58:00 +0200

    creation routes button accueil

--- public/js/index.js
+++ public/js/index.js
@@ -17,3 +17,34 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+ // Cacher le bouton
+ document.querySelector('.show-button').classList.add('hidden');
+    
+ // Script pour l'animation des lettres
+ document.addEventListener('DOMContentLoaded', () => {
+     const letters = document.querySelectorAll('.bonjour-letter');
+     letters.forEach((letter, index) => {
+         setTimeout(() => {
+             letter.classList.add('show');
+             // Si c'est la dernière lettre, montrer le bouton
+             if (index === letters.length - 1) {
+                 document.querySelector('.show-button').classList.remove('hidden');
+                 document.querySelector('.show-button').classList.add('visible');
+             }
+         }, index * 250); // Délai de 250ms entre chaque lettre
+     });
+ });
+
+ // Fonction pour afficher le contenu
+ function showContent() {
+     document.querySelector('.blue-overlay').classList.add('show-blue-overlay');
+     document.querySelector('.red-overlay').classList.add('show-red-overlay');
+     document.querySelector('.show-button').style.display = 'none'; // Cacher le bouton
+     const letters = document.querySelectorAll('.bonjour-letter');
+     letters.forEach((letter, index) => {
+         setTimeout(() => {
+             letter.style.display = 'none'; // Cacher chaque lettre
+         }, 0 + index * 0); // Délai de 3 secondes avant de cacher chaque lettre
+     });
+ }
